30d25728b2500d80ab82abdd891ca270667b003f
mz9464 <mz9464@g.rit.edu>
2020-07-06 13:02:37 -0400

    bot sends welcome and leaving messages directly to server

--- bot.py
+++ bot.py
@@ -12,11 +12,15 @@ async def on_ready():
 async def on_member_join(member):
     print(f'Welcome {member}, the Potato Lord does not hate you.')
     #await client.send(f'Welcome {member}, the Potato Lord does not hate you.')
+    channel = client.get_channel(id=701270246496927797)
+    await channel.send(f'Welcome {member}, the Potato Lord does not hate you.')
 
 @client.event
 async def on_member_remove(member):
     print(f'{member}, begone thot.')
     #await client.send_message(f'{member}, begone thot.')
+    channel = client.get_channel(id=701270246496927797)
+    await channel.send(f'{member}, begone thot.')
 
 
 @client.command(aliases=['read'])
