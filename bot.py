2a406f1978ff90a7f492f977099cdd0176b01a06
mz9464 <mz9464@g.rit.edu>
2020-07-06 00:03:00 -0400

    added welcome and leaving messages

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Hello, world.')
 
+@client.event
+async def on_member_join(member):
+    print(f'Welcome {member}, the Potato Lord does not hate you.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member}, begone thot.')
+
 client.run(config.token)
