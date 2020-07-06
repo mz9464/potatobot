05b7b2cceeca9e6984983605d1cbcfb64000a8e0
mz9464 <mz9464@g.rit.edu>
2020-07-06 03:37:56 -0400

    added a readme, selects a random response from an array

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 import config
 
@@ -7,13 +8,22 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Hello, world.')
 
-
 @client.event
 async def on_member_join(member):
     print(f'Welcome {member}, the Potato Lord does not hate you.')
+    #await client.send(f'Welcome {member}, the Potato Lord does not hate you.')
 
 @client.event
 async def on_member_remove(member):
     print(f'{member}, begone thot.')
+    #await client.send_message(f'{member}, begone thot.')
+
+
+@client.command(aliases=['read'])
+async def readme(ctx):
+    responses = ['This bot was created to help run the dictatorship',
+                 'Hail the Potato Lord',
+                 'idk what you expected as a response']
+    await ctx.send(f'{random.choice(responses)}')
 
 client.run(config.token)
