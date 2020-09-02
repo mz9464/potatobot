69340c0d29bcb38b3e8892a7b87b9305880cbeac
mz9464 <mz9464@g.rit.edu>
2020-09-02 19:56:51 -0400

    refactoring code, all member methods are in a different class

---
+++ cogs/welcome.py
@@ -0,0 +1,62 @@
+import discord
+import person
+import botdata
+import config.client as client
+
+""" 
+Sends a message to the terminal and creates a list 
+of members with the default value of reputation when the bot is online
+"""
+@client.event
+async def on_ready():
+    people = []
+    #client.remove_command("help")
+    print('Hello, world.')
+    x = client.get_all_members()
+    for members in x:
+        people.append(person.Person(members.name, members.id, 0))
+    botdata.load_data(people)
+    activity = discord.Game(name='.info | potatobot')
+    await client.change_presence(activity=activity)
+
+"""
+Sends a welcome message to the server when a member joins and 
+adds them to the list of members 
+:param member: the name of the member
+"""
+@client.event
+async def on_member_join(member):
+    print(f'Welcome {member}, the Potato Lord does not hate you.')
+    channel = client.get_channel(id=701270246496927797)
+    await channel.send(f'Welcome {member.display_name}, the Potato Lord does not hate you.')
+    people.append(person.Person(member.name, member.id, 0))
+    print(member.display_name, " has been added")
+
+"""
+Sends a goodbye message to the server when a member leaves and 
+removes them from the list of members 
+:param member: the name of the member
+"""
+@client.event
+async def on_member_remove(member):
+    print(f'{member}, begone thot.')
+    channel = client.get_channel(id=701270246496927797)
+    await channel.send(f'{member.display_name}, begone thot.')
+    # people.remove(member)
+    # TODO need to finish this
+
+"""
+Sends an info message of the server, introducing the bot
+and listing its commands
+"""
+@client.command()
+async def info(ctx):
+    embed = discord.Embed(title="Potato Bot",
+            description=(f'Hello! I am Potato Bot and my job is to help run the dictatorship of this server\n\n'
+                '**COMMANDS** \n `.rep` [@member] [value] [reason] : changes the rep of a member\n'
+                '.leaderboard : displays the top 5 members with the most rep \n'
+                '.shameboard : displays the top 5 members with the least amount of rep \n'
+                '.checkRep : displays the reputation of a single member \n'),
+            color=ctx.author.color)
+    embed.set_thumbnail(url=ctx.guild.me.avatar_url)
+    await ctx.send(embed=embed)
