3f1ff8575237f3be28dfb44b35f8a760b5baaf1e
mz9464 <mz9464@g.rit.edu>
2020-11-02 22:44:55 -0500

    welcome.py works and is fully documented

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,3 +1,8 @@
+"""
+file: welcome.py
+author: Misty Zheng
+description: bot setup, keeps track of members and contains a custom help command
+"""
 import discord
 import person
 import botdata
@@ -12,7 +17,6 @@ class Welcome(commands.Cog):
     Sends a message to the terminal and creates a list 
     of members with the default value of reputation when the bot is online
     """
-
     @commands.Cog.listener()
     async def on_ready(self):
         print('Hello, world.')
@@ -30,55 +34,56 @@ class Welcome(commands.Cog):
     adds them to the list of members 
     :param member: the name of the member
     """
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = self.client.get_channel(id=751995615382208534)
         embed = discord.Embed(title=f'Welcome {member.display_name}',
                               description='The Potato Lord does not hate you')
         embed.set_thumbnail(url=f'{member.avatar_url}')
-        await channel.send(embed=embed)
-        c.people.append(person.Person(member.name, member.id, 0))
+        for channel in member.guild.channels:
+            if str(channel) == "general":
+                await channel.send(embed=embed)
+        guild = member.guild
+        c.Dict[guild.id].append(person.Person(member.name, member.id, 0))
         print(member.display_name, " has been added")
-        botdata.save_data(c.people)
+        botdata.save_data(c.Dict[guild.id])
 
     """
     Sends a goodbye message to the server when a member leaves and 
     removes them from the list of members 
     :param member: the name of the member
     """
-
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        channel = self.client.get_channel(id=751995615382208534)
+        guild = member.guild
         i=0
-        for p in c.people:
+        for p in c.Dict[guild.id]:
             if (str(p.id) == str(member.id)):
                 print('found')
                 break
             else:
                 i += 1
-        await channel.send(f'{member.display_name}, begone thot.')
-        botdata.delete_member(member, c.people)
-        if i <= len(c.people):
-            c.people.pop(i)
+        for channel in member.guild.channels:
+            if str(channel) == "general":
+                await channel.send(f'{member.display_name}, begone thot.')
+        botdata.delete_member(guild.id, member, c.Dict[guild.id])
+        if i <= len(c.Dict[guild.id]):
+            c.Dict[guild.id].pop(i)
         print(f'{member} has been removed.')
 
     """
     Sends an info message of the server, introducing the bot
     and listing its commands
     """
-
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(title="Potato Bot",
                               description=(
                                   f'Hello! I am Potato Bot and my job is to help run the dictatorship of this server\n\n'
-                                  '**RESERVED COMMANDS** \n .rep [@member] [value] [reason] : changes the rep of a member\n'
-                                  '.jail [@member] [duration] : puts server member in jail \n\n'
-                                  '**REGULAR COMMANDS** \n .leaderboard : displays the top 5 members with the most rep \n'
-                                  '.shameboard : displays the top 5 members with the least rep \n'
-                                  '.checkRep : displays the reputation of a single member \n'),
+                                  '**RESERVED COMMANDS** \n `.rep [@member] [value] [reason]` : changes the rep of a member\n'
+                                  '`.jail [@member] [duration]` : puts server member in jail \n\n'
+                                  '**REGULAR COMMANDS** \n `.leaderboard` : displays the top 5 members with the most rep \n'
+                                  '`.shameboard` : displays the top 5 members with the least rep \n'
+                                  '`.checkRep` : displays the reputation of a single member \n'),
                               color=ctx.author.color)
         embed.set_thumbnail(url=ctx.guild.me.avatar_url)
         await ctx.send(embed=embed)
